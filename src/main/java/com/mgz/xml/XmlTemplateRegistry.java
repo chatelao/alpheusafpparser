9bf50272067664306c6114bdfaebb006267a3fa8
chatelao <2461532+chatelao@users.noreply.github.com>
2026-06-17 13:05:50 +0000

    Fix redundant double quotes in XML output for rules and triplets
    - Corrected attribute construction logic in AfpJacksonXmlWriter.java
    - Updated XmlTemplateRegistry.java to remove redundant quotes in templates and align with writer
    - Added DoubleQuoteBugTest.java to verify the fix and prevent regressions

--- src/main/java/com/mgz/xml/XmlTemplateRegistry.java
+++ src/main/java/com/mgz/xml/XmlTemplateRegistry.java
@@ -79,22 +79,22 @@ public class XmlTemplateRegistry {
     register("FCGCS", new String[] {"<FontCodedGraphicCharacterSetGlobalID codedGraphicCharacterSetGlobalID=\"", "\" codePageGlobalID=\"", "\"/>"});
     register("MFS", new String[] {"<MODCAFunctionSet fctSetID=\"", "\"/>"});
     register("AD", new String[] {"<AreaDefinition reserved2=\"", "\" xOrigin=\"", "\" yOrigin=\"", "\" xSize=\"", "\" ySize=\"", "\"/>"});
-    register("OCNT", new String[] {"<ObjectCount subordinateObjectType=\"", "\" reserved3=\"", "\" numberOfObjectsLow=\"", "\"", "/>"});
+    register("OCNT", new String[] {"<ObjectCount subordinateObjectType=\"", "\" reserved3=\"", "\" numberOfObjectsLow=\"", "", "/>"});
     register("LODTS", new String[] {"<LocalObjectDateAndTimeStamp dateAndTimeStampType=\"", "\" hundreds=\"", "\" tens=\"", "\" dayOfYear=\"", "\" hourOfDay=\"", "\" minuteOfHour=\"", "\" secondOfMinute=\"", "\" hundredthOfSecond=\"", "\"/>"});
     register("UDTS", new String[] {"<UniversalDateAndTimeStamp reserved2=\"", "\" year=\"", "\" monthOfYear=\"", "\" dayOfMonth=\"", "\" hourOfDay=\"", "\" minuteOfHour=\"", "\" secondOfMinute=\"", "\" timeZone=\"", "\" diffHours=\"", "\" diffMinutes=\"", "\"/>"});
     register("OBO", new String[] {"<ObjectByteOffset byteOffset=\"", "\"", "/>"});
     register("OSFO", new String[] {"<ObjectStructuredFieldOffset offsetLow=\"", "\"", "/>"});
     register("OSFE", new String[] {"<ObjectStructuredFieldExtent numberOfSFLow=\"", "\"", "/>"});
-    register("OO", new String[] {"<ObjectOffset", " reserved3=\"", "\" nrOfPrecedingObjectsLow=\"", "\"", "/>"});
+    register("OO", new String[] {"<ObjectOffset", " reserved3=\"", "\" nrOfPrecedingObjectsLow=\"", "", "/>"});
 
     // Structured Fields
     register("PGP1", new String[] {"<PGP_PagePosition_Format1 xOrigin=\"", "\" yOrigin=\"", "\"/>"});
-    register("PTD1", new String[] {"<PTD_PresentationTextDataDescriptor_Format1", "", " xUnitsPerUnitBase=\"", "\" yUnitsPerUnitBase=\"", "\" xSize=\"", "\" ySize=\"", "\"", "/>"});
+    register("PTD1", new String[] {"<PTD_PresentationTextDataDescriptor_Format1", "", "", " xUnitsPerUnitBase=\"", "\" yUnitsPerUnitBase=\"", "\" xSize=\"", "\" ySize=\"", "", "/>"});
 
     // PTOCA Control Sequences
     register("SEC", new String[] {"<SEC_SetExtendedTextColor colorSpace=\"", "\" nrOfBitsComponent1=\"", "\" nrOfBitsComponent2=\"", "\" nrOfBitsComponent3=\"", "\" nrOfBitsComponent4=\"", "\" colorValue=\"", "\"/>"});
-    register("TBM", new String[] {"<TBM_TemporaryBaselineMove page=\"", "\" x=\"", "\" y=\"", "\"", "/>"});
-    register("OVS", new String[] {"<OVS_Overstrike page=\"", "\" x=\"", "\" y=\"", "\"", "/>"});
+    register("TBM", new String[] {"<TBM_TemporaryBaselineMove page=\"", "\" x=\"", "\" y=\"", "", "/>"});
+    register("OVS", new String[] {"<OVS_Overstrike page=\"", "\" x=\"", "\" y=\"", "", "/>"});
     register("RPL", new String[] {"<repeatLength>", "</repeatLength>"});
 
     register("TRN_START", new String[] {"<TRN_TransparentData page=\"", "\" x=\"", "\" y=\"", "\">"});
